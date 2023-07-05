0df4c3c2bdceed94d679a9ca5b6908287de4c551
Ikmetullaev <rzaikmetullaev0@gmail.com>
2023-07-05 17:42:52 +0500

    first commit

---
+++ Funkciya.py
@@ -0,0 +1,85 @@
+'''
+def fun():
+    print('Hello world')
+fun()
+def rza():
+    print("Ikmetullaev Rza")
+rza()
+
+def rza():
+    a=int(input('san: '))
+    b=int(input('san2: '))
+    print(a+b)
+rza()
+def rza():
+    c=int(input("San3: "))
+    d=int(input("sa4: "))
+    print(c*d)
+rza()
+def rza():
+    f=int(input('san5: '))
+    g=int(input("san6 "))
+    print(f//g)
+rza()
+def rza():
+    s=int(input('san7: '))
+    w=int(input('sa8: '))
+    print(s%w)
+rza()
+
+def rza(ol):
+    print(ol)
+rza('Rza')
+
+def rza(a,b):
+    print(a+b)
+a=int(input("san: "))
+b=int(input('san2: '))
+rza(a,b)
+def rza(d,f):
+    print(d-f)
+d=int(input('san3: '))
+f=int(input('san4: '))
+rza(d,f)
+def kobeytiw(g,e):
+    print(g*e)
+g=int(input('san5: '))
+e=int(input('san6: '))
+kobeytiw(g,e)
+def boliw(w,r):
+    print(w//r)
+w=int(input('san7: '))
+r=int(input('san8: '))
+boliw(w,r)
+
+def func():
+    rza=int(input("Jas: "))
+    if rza>10:
+        print("Oqiwga tapsira alasan' !")
+    else:
+        print("Oqiwg'a tapsira almaysan'")
+func()
+
+def func_2():
+    jas=int(input("Jasin': "))
+    print(f"Sen {2023-jas}-jilsan'")
+func_2()
+
+def func():
+    a=int(input("Jasin': "))
+    for i in a:
+        if i==23:
+            print("Malades")
+        else:
+            print(a)
+func()
+'''
+def func():
+    user=[]
+    user.append("Aqlas")
+    user.append("Dawlet")
+    user.append("Bza")
+    user.append("Clug")
+    user.sort()
+    print(user)
+func()
